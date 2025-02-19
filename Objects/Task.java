22b5ec9559cc47bb37cc76df07360e6821bf565d
goIsaiah <goisaiah@outlook.com>
2025-02-19 12:21:51 -0500

    Added new classes

---
+++ Objects/Task.java
@@ -0,0 +1,9 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Task {
+    private int id; // This can be changed to a String
+    private String name;
+    private Team team;
+
+}
