027eeb8aaad0cfc7163dabb3549219c61f042184
SupaHacka02 <mherericgyulumyan@gmail.com>
2025-02-20 22:34:13 -0500

    Modified db

---
+++ db/db.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS projects;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL 
+);
+
+CREATE TABLE projects (
+    id SERIAL PRIMARY KEY,
+    projectName VARCHAR(150) NOT NULL,
+    projectOwner int NOT NULL,
+    FOREIGN KEY (projectOwner) REFERENCES users(id) ON DELETE CASCADE
+);
